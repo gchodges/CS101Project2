2f4f25791c4c2f4fecbda07a23198aca21fc30ac
gchodges <garretthodges16@gmail.com>
2022-10-02 17:52:12 -0400

    Add inFiles

--- p2.cpp
+++ p2.cpp
@@ -7,6 +7,12 @@ using namespace std;
 
 int main(int argc,char* argv[]){
 
+    ifstream inFile1;
+
+    ifstream inFile2;
+
+    inFile1.open(argv[3]);
+    inFile2.open(argv[4])
 
 
     return 0;
