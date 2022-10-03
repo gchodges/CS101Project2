876b420e79d048015b7ff559f381752768a3a07a
gchodges <garretthodges16@gmail.com>
2022-10-02 21:51:05 -0400

    add mergesort

--- p2.cpp
+++ p2.cpp
@@ -2,28 +2,164 @@
 #include <fstream>
 # include <string>
 #include <cctype>
+#include <cstring>
 
 using namespace std;
 
-int main(int argc,char* argv[]){
+void Merge(int numbers[], int i, int j, int k) {
+   int mergedSize;                            // Size of merged partition
+   int mergePos;                              // Position to insert merged number
+   int leftPos;                               // Position of elements in left partition
+   int rightPos;                              // Position of elements in right partition
+   int* mergedNumbers = nullptr;
 
+   mergePos = 0;
+   mergedSize = k - i + 1;
+   leftPos = i;                               // Initialize left partition position
+   rightPos = j + 1;                          // Initialize right partition position
+   mergedNumbers = new int[mergedSize];       // Dynamically allocates temporary array
+                                              // for merged numbers
+   
+   // Add smallest element from left or right partition to merged numbers
+   while (leftPos <= j && rightPos <= k) {
+      if (numbers[leftPos] < numbers[rightPos]) {
+         mergedNumbers[mergePos] = numbers[leftPos];
+         ++leftPos;
+      }
+      else {
+         mergedNumbers[mergePos] = numbers[rightPos];
+         ++rightPos;
+         
+      }
+      ++mergePos;
+   }
+   
+   // If left partition is not empty, add remaining elements to merged numbers
+   while (leftPos <= j) {
+      mergedNumbers[mergePos] = numbers[leftPos];
+      ++leftPos;
+      ++mergePos;
+   }
+   
+   // If right partition is not empty, add remaining elements to merged numbers
+   while (rightPos <= k) {
+      mergedNumbers[mergePos] = numbers[rightPos];
+      ++rightPos;
+      ++mergePos;
+   }
+   
+   // Copy merge number back to numbers
+   for (mergePos = 0; mergePos < mergedSize; ++mergePos) {
+      numbers[i + mergePos] = mergedNumbers[mergePos];
+   }
+   delete[] mergedNumbers;
+}
+
+void MergeSort(int numbers[], int i, int k) {
+   int j;
+   
+   if (i < k) {
+      j = (i + k) / 2;  // Find the midpoint in the partition
+      
+      // Recursively sort left and right partitions
+      MergeSort(numbers, i, j);
+      MergeSort(numbers, j + 1, k);
+      
+      // Merge left and right partition in sorted order
+      Merge(numbers, i, j, k);
+   }
+}
+
+// int main() {
+//    int numbers[] = { 10, 2, 78, 4, 45, 32, 7, 11 };
+//    const int NUMBERS_SIZE = 8;
+//    int i;
+   
+//    cout << "UNSORTED: ";
+//    for(i = 0; i < NUMBERS_SIZE; ++i) {
+//       cout << numbers[i] << " ";
+//    }
+//    cout << endl;
+   
+//    MergeSort(numbers, 0, NUMBERS_SIZE - 1);
+   
+//    cout << "SORTED: ";
+//    for(i = 0; i < NUMBERS_SIZE; ++i) {
+//       cout << numbers[i] << " ";
+//    }
+//    cout << endl;
+   
+//    return 0;
+// }
+
+int main(int argc,char* argv[]){
+    cout << "im in main" << endl;
     ifstream inFile1;
 
     ifstream inFile2;
 
     inFile1.open(argv[2]);
-    inFile2.open(argv[3);
+    inFile2.open(argv[3]);
 
-    if (argv[1] == "i"){
+    if (argv[1] == "i\0"){
+        cout << "i got here";
+        int arr1[20000];
+        int arr2[20000];
 
+        int arr1Size, arr2Size;
 
+        int i = 0;
+
+        while (inFile1 >> arr1[i]){
+
+            i++;
+        }
+
+        arr1Size = i;
+        i = 0;
+
+        while (inFile2 >> arr2[i]){
+
+            i++;
+        }
+
+        arr2Size = i;
+
+        MergeSort(arr1,0,arr1Size);
+        MergeSort(arr2,0,arr2Size);
+
+        for (int one = 0; one < arr1Size;one++){
+            for (int two = 0;two < arr2Size;two++){
+                if (arr1[one] == arr2[two]){
+                    cout << arr1[one] << endl;
+                }
+
+            }
+
+        }
 
     }
 
     if (argv[1] == "s"){
+        cout << " im in string";
+        string arr1[20000];
+        string arr2[20000];
+
+        int i = 0;
+
+        while (inFile1 >> arr1[i]){
+
+            i++;
+        }
+
+        i=0;
+
+        while (inFile2 >> arr2[i]){
+
+            i++;
+        }
 
 
-        
     }
 
     return 0;
